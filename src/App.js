1638e95b6addd353eba509466e95ae40ca718e87
filippopendyk <filipos28511@gmail.com>
2022-10-13 16:55:43 -0400

    testing how the routing works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route }  from 'react-router-dom';
+import React from 'react';
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return <BrowserRouter> 
+    <Routes>
+        <Route path='/' element={<div>home page</div>}/>
+        <Route 
+          path='testing' 
+          element={
+            <div>
+              <h2>testing</h2>
+            </div>
+          }
+        />
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
