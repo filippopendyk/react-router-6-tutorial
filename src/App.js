b4102faf75d2f708fa0f81b913591a7b52dda0a1
filippopendyk <filipos28511@gmail.com>
2022-10-13 23:07:41 -0400

    creating nav comp files

--- src/App.js
+++ src/App.js
@@ -8,10 +8,14 @@ import Error from './pages/Error';
 function App() {
   return <BrowserRouter> 
     <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='about' element={<About/>} />
-        <Route path='products' element={<Products/>} />
-        <Route path='*' element={<Error />} />
+        <Route path='/' element={<Home/>}>
+          <Route path='about' element={<About/>} />
+          <Route path='products' element={<Products/>} />
+          <Route path='*' element={<Error />} />
+        </Route>
+        <Route path='dashboard' element={<div>Dashboard</div>}>
+          <Route path='stats' element={<div>Stats</div>}/>
+        </Route>
     </Routes>
   </BrowserRouter>;
 }
