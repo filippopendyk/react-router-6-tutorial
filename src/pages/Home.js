2bd2a3b67d70da98b677fd43c32a8ff54b938dba
filippopendyk <filipos28511@gmail.com>
2022-10-14 11:40:22 -0400

    sets up single products dynamic routing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
-import { Link, Outlet } from 'react-router-dom';
-
 const Home = () => {
   return (
-    <section className='section'>
-      <h2>Home Page</h2>
-      <Outlet/>
-    </section>
+    <>
+      <section className='section'>
+        <h2>Home Page</h2>
+      </section>
+    </>
   );
 };
 export default Home;
